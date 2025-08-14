48c1b387c055dc3ea309d9885f7007c8c65359c1
MrGBroward <bruce@gforcepressurewashing.com>
2025-08-14 08:36:02 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,6 @@
-import App from './App.jsx';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx"; // exact casing
+
+createRoot(document.getElementById("root")).render(<App />);
+
