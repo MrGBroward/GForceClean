88973eaffd6788e5eb557c10413d1f04e1e1d5ff
MrGBroward <bruce@gforcepressurewashing.com>
2025-08-13 20:15:30 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+```jsx
+  export default function App() {
+    return (
+      <div style={{ padding: '1rem', fontFamily: 'sans-serif' }}>
+        <h1>G‑Force Exterior Cleaning</h1>
+        <p>Site is live! We’ll add full content soon.</p>
+      </div>
+    );
+  }
+  ```
