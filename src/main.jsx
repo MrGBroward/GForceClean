a590c86b36b3f8f6530293997d56731a25c94073
MrGBroward <bruce@gforcepressurewashing.com>
2025-08-15 14:47:49 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx"; // exact casing
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(<App />);
+const root = createRoot(document.getElementById('root'))
+root.render(<App />)
 
