7b47149b9b337f31eb3be035dabdab0cee80af03
MrGBroward <bruce@gforcepressurewashing.com>
2025-08-18 21:55:30 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,7 @@ function GallerySection() {
           <img src="/images/before-and-after-house-2.jpg" alt="House cleaning – before & after" style={imgStyle} />
           <img src="/images/before-and-after-bulldozer-2.jpg" alt="Heavy equipment cleaning – before & after" style={imgStyle} />
         </div>
-        <p style={{ ...p, marginTop: 10 }}>Tip: add more images to <code>/public/images/</code> and reference them here.</p>
+        <p style={{ ...p, marginTop: 10 }}></p>
       </div>
     </section>
   );
